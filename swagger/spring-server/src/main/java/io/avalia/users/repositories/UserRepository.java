fddf86c7b50454e891749d24e29bbd5635e550b9
alexandre.gabrielli <alexandre.gabrielli@heig-vd.ch>
2019-12-11 10:54:44 +0100

    first fonctionnal project

---
+++ swagger/spring-server/src/main/java/io/avalia/users/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package io.avalia.users.repositories;
+
+import io.avalia.users.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Olivier Liechti on 26/07/17.
+ */
+public interface UserRepository extends CrudRepository<UserEntity, Long>{
+
+}
