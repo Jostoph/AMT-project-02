3b6eebc838d75064f48ed265b403458bf8edfd7f
Christoph Rueff <christoph.rueff@gmail.com>
2020-01-15 11:52:02 +0100

    Add run-all.sh script with updated docker-compose etc...

---
+++ run-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# mvn clean install -f ./microservices/auth-service/spring-server/pom.xml
+# cp ./microservices/auth-service/spring-server/target/swagger-spring-1.0.0.jar ./microservices/auth-service/images/alpine-java/
+
+# mvn clean install -f ./microservices/shop-service/spring-server/pom.xml
+# cp ./microservices/shop-service/spring-server/target/swagger-spring-1.0.0.jar ./microservices/shop-service/images/alpine-java/
+
+cd ./topology
+docker-compose up --build
